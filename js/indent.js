14b62ec7b4119e3ccd94c48ccf1e6f34da371b77
carminaburanaonline <ubindini@gmail.com>
2026-03-28 16:16:06 +0100

    Started work on neume.html

--- js/indent.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $(".verse").each(function() {
-    var indent = $(this).data("indent");
-    $(this).children(".verse-text").css("padding-left", "calc(" + indent + " * 20px");
-  });
-});
