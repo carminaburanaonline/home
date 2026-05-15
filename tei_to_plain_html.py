b5d4e0c9c43b67c9e9a0ce8cfd426cf2f26badf3
carminaburanaonline <ubindini@gmail.com>
2026-05-15 13:28:19 +0200

    Corrections on CB138, updated html files

---
+++ tei_to_plain_html.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# Rendering the .tei files as html, with basic formatting (italics, bold) and no musical information
+
+import os
+
+
+def to_paragraph(p):
+	# parsing this element as paragraph (no more tags needed)
+	res = ""
+	elements = p.xpath(".//w|.//pc[@resp='#editor']")
+	if len(elements):
+		for el in p.xpath(".//w|.//pc[@resp='#editor']"):
+			if el.xpath("./ancestor::rdg") or el.xpath("./ancestor::note"):
+				# exclude words in app-readings or app-notes
+				pass
+			else:
+				syllables = el.xpath("./seg[@type='syll']|./app[@type='neume']/lem/seg[@type='syll']")
+				if not len(syllables):
+					res += el.text
+				else:
+					res += "".join([syllable.text for syllable in syllables])
+			res += " "
+		for pc in [',', ';', ':', '.', '!', '?', '»']:
+			for i in range(3):
+				res = res.replace(f" {pc}", pc)
+		for pc in ['«']:
+			for i in range(3):
+				res = res.replace(f"{pc} ", pc)
+	else:
+		if p.text is None:
+			res = ""
+		else:
+			res = p.text
+
+	return res
+
+
+def to_html(tei_file):
+	res = "<html>\n<body>\n"
+	from lxml import etree
+	xml = etree.parse('tei/' + tei_file)
+	for el in xml.xpath(".//stage|.//p|.//lg"):
+		if el.tag == 'stage':
+			res += "<p><i>"
+			res += to_paragraph(el)
+			res += "</i></p>\n"
+		elif el.tag == 'p':
+			res += "<p>"
+			res += to_paragraph(el)
+			res += "</p>\n"
+		elif el.tag == 'lg':
+			if 'n' in el.attrib.keys():
+				res += "<p><b>" + el.attrib['n'] + "</b></p>\n"
+			for head in el.findall('head'):
+				res += "<p><b>" + head.text + "</b></p>\n"
+			for line in el.findall('l'):
+				res += "<p>"
+				hemistichs = line.xpath("seg[@type='hemistich']")
+				if len(hemistichs):
+					res += " ".join([to_paragraph(hemistich) for hemistich in hemistichs])
+				else:
+					res += to_paragraph(line)
+				res += "</p>\n"
+	res += "</html>\n</body>\n"
+	return res
+
+
+if __name__ == "__main__":
+	for file in os.listdir('tei'):
+		if file.endswith('.tei'):
+			html_file = open('html/' + file[:-3] + "html", 'w')
+			print(file)
+			html_file.write(to_html(file))
+			html_file.close()
