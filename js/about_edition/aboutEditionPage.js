e063d7e75e35e6be2a66a9b16c754ddd91f6fa1f
carminaburanaonline <ubindini@gmail.com>
2026-06-22 06:19:00 +0200

    Clean up

--- js/about_edition/aboutEditionPage.js
+++
@@ -1,8 +0,0 @@
-// Entry point for the page "About the edition"
-
-
-
-export async function renderSourcePage() {
-  currentLang;
-  const content = SVGfiles?'templates/item_core_view_svg.html':'templates/item_core_view.html'
-}
